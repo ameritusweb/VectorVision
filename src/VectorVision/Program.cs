10bb8bf72fc5dcc40bfa1bd89e7408a47a7ce929
Scott Seto <scott.seto@outlook.com>
2025-03-23 14:07:57 -0400

    Update image path.

--- src/VectorVision/Program.cs
+++ src/VectorVision/Program.cs
@@ -5,7 +5,7 @@
         static async Task Main(string[] args)
         {
             // Get all pet images
-            string[] imagePaths = Directory.GetFiles("E:\\PetImages\\Animals", "*.jpg");
+            string[] imagePaths = Directory.GetFiles("E:\\PetImages\\Animals", "*.jpg", SearchOption.AllDirectories);
 
             // Create POC with batch size of 5
             var poc = new PetOrderer(
